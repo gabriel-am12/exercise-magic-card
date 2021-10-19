d7499f4c3f03f1554f64a4a160200e9477f8dd7c
Gabriel A <gabrielaug4@gmail.com>
2021-10-19 16:59:25 -0300

    ex.01

--- tests/magic.test.js
+++ tests/magic.test.js
@@ -10,11 +10,24 @@ const expected = {
 };
 
 describe(' Testa a função getMagicCard', () => {
-  it('Deve retornar um objeto com as propriedades esperadas', () => {
+  it('Deve retornar um objeto com as propriedades esperadas', async () => {
     const getMagicCardSimulator = trybeSimulator(magic, 'getMagicCard');
-    await getMagicCardSimulator('130550');
+    const response = await getMagicCardSimulator('130550');
 
-    // implemente seus testes aqui    
-   
+    // implemente seus testes aqui
+    const {
+      name,
+      manaCost,
+      types,
+      subtypes,
+      rarity
+    } = response; 
+
+   expect(response).toEqual(expected);
+   expect(types).toBeInstanceOf(Array);
+   expect(subtypes).toHaveLength(2);
+   expect(rarity).toBe('Uncommon');
+   expect(name).toBe("Ancestor's Chosen");
+   expect(manaCost).toBe('{5}{W}{W}');
   });
 });
